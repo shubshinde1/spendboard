427ad4dc8bc2455ebe7c22bc928638f9a97b132c
shushinde1 <shushinde111@gmail.com>
2023-10-05 16:55:16 +0530

    chnge

--- src/App.js
+++ src/App.js
@@ -2,28 +2,31 @@ import React from 'react';
 import { Layout } from 'antd';
 import './App.css';
 
-const { Header, Footer, Sider, Content } = Layout;
+const { Header, Footer, Sider, Content} = Layout;
 function App() {
   return (
     <div className="App">
       <Layout className=' mx-auto'>
-        <Sider style={{ background: "purple" }} className="h-[90vh]  m-3 rounded-lg w-28 md:w-auto">
-          <div className='bg-red-300'>
-
-          </div>
-          Sidersdfghjk
-        </Sider>
-        <Layout className='space-y-3 my-3'>
-          <Header className="bg-red-400 rounded-lg">Header</Header>
-          <Content className='bg-blue-300 rounded-lg'>Content</Content>
-          <Footer className='bg-red-700 text-white rounded-lg'>Footer</Footer>
+        <Layout className='mx-3 mt-3'>
+          <Header className="bg-red-400 rounded-lg">
+            <div className='bg-emerald-400 w-28 mx-auto'>
+              <img src={'img'} alt="" />
+            </div>
+          </Header>
         </Layout>
-        <Sider style={{ background: "red" }} className="h-[90vh]  m-3 rounded-lg w-28 md:w-auto">
-          <div className='bg-red-300 '>
+        <Layout>
+          <Sider style={{ background: "purple" }} className="h-[90vh]  m-3 rounded-lg w-28 md:w-auto">Sidebar</Sider>
+          <Layout className='space-y-3 my-3'>
+            <Content className='bg-blue-300 rounded-lg'>Content</Content>
+            <Footer className='bg-red-700 text-white rounded-lg'>Footer</Footer>
+          </Layout>
+          <Sider style={{ background: "red" }} className="h-[90vh]  m-3 rounded-lg w-28 md:w-auto">
+            <div className='bg-red-300 '>
 
-          </div>
-          Sidersdfghjk
-        </Sider>
+            </div>
+            Sidersdfghjk
+          </Sider>
+        </Layout>
       </Layout>
     </div>
   );
